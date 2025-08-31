0cca9446c9b9821a0168d286bce9c9bcd27885d7
Kartik <kartik200421@gmail.com>
2025-08-31 18:17:47 +0530

    added trpc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,26 @@
 import prisma from "@/lib/db";
-import { Jost } from "next/font/google";
+import { trpc } from "@/trpc/client";
+import { caller } from "@/trpc/server";
 
 export default async function Page() {
+  const data = await caller.hello({ text: "kartik bhatt" });
   const user = await prisma.user.findMany();
   const posts = await prisma.post.findMany();
   return (
     <div>
       {JSON.stringify(user)} {JSON.stringify(posts)}
+      {JSON.stringify(data)}
     </div>
   );
 }
+
+// "use client";
+
+// import { trpc } from "@/trpc/client";
+
+// export default function Page() {
+//   const { data, isLoading } = trpc.hello.useQuery({ text: "world" });
+
+//   if (isLoading) return <div>Loading...</div>;
+//   return <div>{JSON.stringify(data)}</div>;
+// }
