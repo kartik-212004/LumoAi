fbd25dd9bbd2ec6187e4393ab8802180d1ea4f62
Kartik <kartik200421@gmail.com>
2025-09-01 16:01:13 +0530

    added sandboxes

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,16 @@
 import { inngest } from "./client";
+import { Sandbox } from "@e2b/code-interpreter";
 import { openai, createAgent, Agent } from "@inngest/agent-kit";
+import { getSandbox } from "./utils";
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
 
   async ({ event, step }) => {
+    const sandboxId = await step.run("get-sandbox-id", async () => {
+      const sandbox = await Sandbox.create("vibe-nextjs-testkartik");
+      return sandbox.sandboxId;
+    });
     const summerizer = createAgent({
       name: "code-agent",
       system:
@@ -16,7 +22,12 @@ export const helloWorld = inngest.createFunction(
       `Summerize the following text : ${event.data.value}}`
     );
     console.log(output);
+    const sandboxUrl = await step.run("get-sandbox-url", async () => {
+      const sandbox = await getSandbox(sandboxId);
+      const host = sandbox.getHost(3000);
+      return `https://${host}`;
+    });
 
-    return { message: `Hello ${event.data.value}!` };
+    return { output, sandboxUrl };
   }
 );
