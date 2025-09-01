9a50336cd7a129e4a001418ae85c6e4513dd39c1
Kartik <kartik200421@gmail.com>
2025-09-01 13:48:19 +0530

    added opeapi in inngest

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,25 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import { trpc } from "@/trpc/client";
 import { toast } from "sonner";
+import { useState } from "react";
 
 export default function Page() {
+  const [value, setValue] = useState("");
   const invoke = trpc.invoke.useMutation({
     onSuccess: () => toast.success("Backgriund Job Started"),
   });
 
   return (
     <div className="p-4">
+      <Input
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
       <Button
-        onClick={() => invoke.mutate({ text: "kartik" })}
+        onClick={() => invoke.mutate({ value: value })}
         disabled={invoke.isPending}
       >
         click me
--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,12 +1,22 @@
 import { inngest } from "./client";
-
+import { openai, createAgent, Agent } from "@inngest/agent-kit";
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
+
   async ({ event, step }) => {
-    await step.sleep("wait-a-moment", "5s");
-    await step.sleep("Processing", "5s");
-    await step.sleep("Almost there", "5s");
-    return { message: `Hello ${event.data.email}!` };
+    const summerizer = createAgent({
+      name: "code-agent",
+      system:
+        "You are an expert Next.js developer. You write readable, maintainable code. You write simple next.js and react snippets",
+      model: openai({ model: "gpt-4o" }),
+    });
+
+    const { output } = await summerizer.run(
+      `Summerize the following text : ${event.data.value}}`
+    );
+    console.log(output);
+
+    return { message: `Hello ${event.data.value}!` };
   }
 );
