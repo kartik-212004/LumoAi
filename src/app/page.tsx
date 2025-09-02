b4300070a6bf7ed93e48209df6d57e466e077f18
Kartik <kartik200421@gmail.com>
2025-09-02 15:52:37 +0530

    messages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,13 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { trpc } from "@/trpc/client";
-import { toast } from "sonner";
 import { useState } from "react";
+import { useQuery } from "@tanstack/react-query";
 
 export default function Page() {
   const [value, setValue] = useState("");
-  const invoke = trpc.invoke.useMutation({
-    onSuccess: () => toast.success("Backgriund Job Started"),
-  });
+  const createMessage = trpc.messages.create.useMutation();
+  const { data: messages } = trpc.messages.getMany.useQuery();
 
   return (
     <div className="p-4">
@@ -19,14 +18,12 @@ export default function Page() {
         onChange={(e) => setValue(e.target.value)}
       />
       <Button
-        onClick={() => invoke.mutate({ value: value })}
-        disabled={invoke.isPending}
+        onClick={() => createMessage.mutate({ value: value })}
+        disabled={createMessage.isPending}
       >
         click me
       </Button>
-
-      {invoke.error && <p>Error: {invoke.error.message}</p>}
-      {invoke.isSuccess && <p>Job sent successfully!</p>}
+      {JSON.stringify(messages, null, 2)}
     </div>
   );
 }
